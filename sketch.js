6f3825a60d98719cb7174ae7b835afc691d05f3c
Prasanna <chnp19@gmail.com>
2020-10-29 22:31:24 +0800

    Added Sling Code

--- sketch.js
+++ sketch.js
@@ -39,7 +39,13 @@ function setup(){
     bird = new Bird(100,100);
 
     log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    slingshot = new Slingshot(bird.body,{x:200,y:100});
+}
+function mouseDragged() {
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+}
+function mouseReleased() {
+    slingshot.fly();
 }
 
 function draw(){
@@ -64,5 +70,5 @@ function draw(){
     bird.display();
     platform.display();
     log6.display();
-    chain.display();    
+    slingshot.display();    
 }
